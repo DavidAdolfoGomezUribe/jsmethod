9472625abce10c5c113e0024cb46eaa9bfae7240
DavidAdolfoGomezUribe <96323893+DavidAdolfoGomezUribe@users.noreply.github.com>
2025-03-31 11:26:03 -0500

    asdasd

---
+++ clases/asyncawait para el manejo de asincronia/main.js
@@ -0,0 +1,84 @@
+console.log("ok")
+
+function abriVehiculo(marca){
+
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(function(){
+            if (marca === "Mazda") {
+                console.log("...abrir vehiculo " + marca)
+                resolve(marca)
+            } else {
+                console.log("...no abrir vehiculo " + marca)
+                reject("error al abrir")
+            }
+        },3000)
+        
+
+    })
+
+
+}
+
+function encendervehiculo(marca){
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+
+            if (marca === "Mazda" ) {
+                console.log("...encender vehiculo " + marca)
+                resolve(marca)
+            } else {
+                
+                console.log("...no abrir vehiculo " + marca)
+                reject("erro al encencer vehiculo")
+            }
+
+            
+        },4000)
+        
+    })
+}
+
+function encenderLuces(marca){
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){
+            console.log("se encienden las luces de " + marca)
+            resolve("ok")    
+        })
+        
+    },5000)
+    
+}
+
+
+// abriVehiculo("Mazda").then((encendervehiculo)).then(encenderLuces).then(function go() {
+//     console.log("go");
+// })
+// .catch(function(error){
+//     console.log(error)
+// }
+
+// ).finally( function () {
+//     console.log("apor enpanadaas")
+    
+// }
+// )
+
+
+
+
+async function ponerEnMarcha(marca) {
+    try {
+        
+        var marca1 = await abriVehiculo(marca)
+        var marca2 = await encendervehiculo(marca1)
+        encenderLuces(marca2).then(()=>{
+            console.log("arrancar")
+
+        })
+    } catch (error) {
+        console.log(error);    
+    }
+}
+
+ponerEnMarcha("Mazda")
